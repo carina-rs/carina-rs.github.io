b6d87e55f43a108ee86c045029591a9810b14f92
mizzy <mizzy@users.noreply.github.com>
2026-03-07 09:00:08 +0000

    deploy: carina-rs/carina@922c64a6b0dd099f50e1a8ef5689a31c4c64c920

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-84652dd9.js',
+                path_to_root + 'searchindex-07d0c2b7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
